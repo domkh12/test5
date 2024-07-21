0d86542a003085580a2b55ba3a04d7dd8d65987a
EI CHANUDOM <chanudomei122@gmail.com>
2024-07-21 21:21:47 +0700

    m

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -7,23 +7,26 @@ const SEO = ({ title, description, keywords, url, image }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+
+      {/* Open Graph / Facebook */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image} />
+      <meta property="og:site_name" content="Showcase" />
+
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
-      <meta name="telegram:card" content="summary_large_image" />
-      <meta name="telegram:title" content={title} />
-      <meta name="telegram:description" content={description} />
-      <meta name="telegram:image" content={image} />
-      <meta name="github:card" content="summary_large_image" />
-      <meta name="github:title" content={title} />
-      <meta name="github:description" content={description} />
-      <meta name="github:image" content={image} />
+
+      {/* Additional Meta Tags */}
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <link rel="canonical" href={url} />
     </Helmet>
   );
 };
