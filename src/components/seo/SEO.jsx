9e0b211dbca35393be1f03b7fa0f61be302026c6
EI CHANUDOM <chanudomei122@gmail.com>
2024-07-21 21:40:42 +0700

    m

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const SEO = ({ title, description, keywords, url, image }) => {
   return (
@@ -8,25 +8,11 @@ const SEO = ({ title, description, keywords, url, image }) => {
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
 
-      {/* Open Graph / Facebook */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content={`website`} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
-      <meta property="og:site_name" content="Showcasesssssssssssssss" />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-
-      {/* Canonical URL */}
-      <link rel="canonical" href={url} />
+      <meta property="og:image" content={image} />      
     </Helmet>
   );
 };
